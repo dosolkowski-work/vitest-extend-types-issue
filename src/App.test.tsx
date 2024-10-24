cc83e06d29947d8dd796017b50bd2a7c939311c0
David Osolkowski <david.osolkowski@vynedental.com>
2024-10-24 09:55:16 -0400

    Adding vitest-axe breaks everything in weird ways

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,16 @@
 import { App } from "App";
 import { render, screen } from "@testing-library/react";
+import { accessibilityViolationsCheck } from "setupTests";
+
+let component: HTMLElement | undefined;
 
 function renderComponent() {
-    render(<App />);
+    component = render(<App />).container;
 }
 
 describe("The application", () => {
+    afterEach(async () => await accessibilityViolationsCheck(component));
+
     it("renders some text", async () => {
         renderComponent();
 
--- src/example-vitest.d.ts
+++ src/example-vitest.d.ts
@@ -1,10 +1,11 @@
 import "vitest";
+import type { AxeMatchers } from "vitest-axe";
 
 interface CustomMatchers {
     toBeFrob: () => void;
 }
 
 declare module "vitest" {
-    interface Assertion<T = any> extends CustomMatchers {}
-    interface AsymmetricMatchersContaining extends CustomMatchers {}
+    interface Assertion<T = any> extends CustomMatchers, AxeMatchers {}
+    interface AsymmetricMatchersContaining extends CustomMatchers, AxeMatchers {}
 }
